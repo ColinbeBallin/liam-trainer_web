ee8864aac60101d9c153e4bab9389583225e1b5f
Colin Franklin <colinfranki05@gmail.com>
2024-12-23 04:53:29 -0700

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Test, this is Liam's super cool website, where we will be seeing naked dudes!!</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
